47cf5c5673587aaa7a001f279e2dc969ad852c01
RashmiImasha <rimasha317@gmail.com>
2025-11-29 22:43:32 +0530

    update code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import sys, asyncio
-from app.test.test_image import generate_test_dataset
+# from app.test.test_image import generate_test_dataset
 
 from app.routes.chat_route import router as chat_route
 from app.routes.category_route import router as category_route
@@ -74,5 +74,5 @@ if __name__ == "__main__" :
         uvicorn.run(app, host = "0.0.0.0", port = 9090, log_level = "info")
 
 
-    # generate_test_dataset(test_size=25)
-    # uvicorn.run(app, host = "0.0.0.0", port = 9090, log_level = "info")
+# generate_test_dataset(test_size=25)
+# uvicorn.run(app, host = "0.0.0.0", port = 9090, log_level = "info")
